acb0b8fe4cd2ae532dfa7235f67eedac247d7443
belgoudene <belgoudene@gmail.com>
2017-11-12 22:40:26 +0100

    [MHE] ajout de article

---
+++ test/src/main/java/com/test/liv/test/Article.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.test.liv.test;
+
+/**
+ * @author belgoudene
+ *
+ */
+public class Article {
+
+	private Integer id;
+	
+	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public final Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public final void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public final String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public final void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
--- test/src/main/java/com/test/liv/test/Test.java
+++ test/src/main/java/com/test/liv/test/Test.java
@@ -15,8 +15,14 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Article ar = new Article();
+		
+		ar.setId(25);
+		ar.setName("table");
+		
+		
 		System.out.println("Mon premier projet !");
-		System.out.println("Test 1 !");
+		System.out.println(ar);
 	}
 
 }
